23fd211e5275279dd034cc48fd5202560c5b0348
Vywren <119077371+Vywren@users.noreply.github.com>
2024-07-04 23:42:09 -0400

    moved login credentials to text file
    whoops

--- admin_commands.py
+++ admin_commands.py
@@ -1,11 +1,13 @@
 import psycopg2
 def access_command_panel(command):
-    conn = psycopg2.connect(database = "Bracket", 
-                        user = "postgres", 
-                        host= 'localhost',
-                        password = "2987tubby2987",
-                        port = 5432)
+    account =  open("account_information.txt","r")
+    conn = psycopg2.connect(database = account.readline(), 
+                        user = account.readline(), 
+                        host= account.readline(),
+                        password = account.readline(),
+                        port = account.readline())
     cur = conn.cursor()
+    account.close()
     # Execute a command:
     cur.execute(command)
     # Make the changes to the database  persistent
@@ -14,15 +16,18 @@ def access_command_panel(command):
     cur.close()
     conn.close()
 
-def new_player():
-    conn = psycopg2.connect(database = "Bracket", 
-                        user = "postgres", 
-                        host= 'localhost',
-                        password = "2987tubby2987",
-                        port = 5432)
+def new_player(first_name, last_name, wins, losses, draws, email, phone_number):
+    account =  open("account_information.txt","r")
+    conn = psycopg2.connect(database = str(account.readline().replace('\n', '')), 
+                        user = str(account.readline().replace('\n', '')), 
+                        host = str(account.readline().replace('\n', '')),
+                        password = str(account.readline().replace('\n', '')),
+                        port = int(account.readline()))
+    account.close()
+
     cur = conn.cursor()
     # Execute a command:
-    cur.execute("INSERT INTO players (first_name, last_name, wins, losses, draws, email, phone_number) VALUES (%s, %s, %s, %s, %s, %s, %s)", ("john", "smith", 0, 0, 0, "john.smith@gmail.com", 203-555-5555))
+    cur.execute("INSERT INTO players (first_name, last_name, wins, losses, draws, email, phone_number) VALUES (%s, %s, %s, %s, %s, %s, %s)", (first_name, last_name, wins, losses, draws, email, phone_number))
     # Make the changes to the database  persistent
     conn.commit()
     # Close cursor and communication with the database
@@ -30,8 +35,8 @@ def new_player():
     conn.close()
    
 def new_tournament():
-    print("Starting new tournament: \n")\
+    print("Starting new tournament: \n")
 
 
 
-new_player()
+new_player("jenny", "smith", 0, 0, 0, "jenny.smith@gmail.com", "203-555-5555")
