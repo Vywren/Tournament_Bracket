bb98c6f540016013e4cea41892d19f0e5bbd1827
Vywren <119077371+Vywren@users.noreply.github.com>
2024-07-17 23:18:13 -0400

    Started using flask
    will most likely ditch all non flask using python files in favor of sqlalchemy

---
+++ flaskApp.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__,template_folder = 'templates')
+
+@app.route("/")
+def do_stuff():
+    return render_template('home.html')
+@app.route("/login",methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["usr"]
+        email = request.form["email"]
+        return redirect(url_for("post_log"))
+    else:
+        return render_template('login.html')
+@app.route("/login/post_log/")
+def post_log():
+    return render_template('post_log.html')
+if __name__ == "__main__":
+    app.run(debug = True)
