b8bd13726bc5188bf62dce8df3cc730334efb1db
Vywren <wolfiethehusky@gmail.com>
2024-10-22 01:08:41 -0400

    room table implemented succesfully

--- testbench.py
+++ testbench.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String 
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey 
 from sqlalchemy.orm import sessionmaker 
 from sqlalchemy.ext.declarative import declarative_base 
   
@@ -13,26 +13,56 @@ sql_session = Session()
 Base = declarative_base() 
   
   
+class single_elim_room(Base):
+    __tablename__ = 'single_elim_room'
+    room_number = Column(Integer, primary_key = True)
+    def __init__(self, room_number):
+        self.room_number = room_number
+    def __repr__(self):
+            return f"({self.room_number})"
+
 class users(Base):
     __tablename__ = 'users'
-    username = Column(Integer)
-    email = Column(Integer, primary_key = True)
-  
-  
+    email = Column(String, primary_key = True)
+    username = Column(String)
+    in_room = Column(Integer, ForeignKey(single_elim_room.room_number))
+    def __init__(self, email, username, in_room):
+        self.email = email
+        self.username = username
+        self.in_room = in_room
+    def __repr__(self):
+            return f"({self.email, self.username, self.in_room})"
+
 # Create the example table 
+Base.metadata.drop_all(engine) 
 Base.metadata.create_all(engine) 
   
-results = sql_session.query(users).filter(users.email == "dylan@gmail.com").first() 
-  
+sql_session.add(single_elim_room(room_number = 0)) 
+sql_session.add(users(email = "dylan@gmail.com",username = "bob",in_room = 0)) 
+sql_session.commit() 
+
+
+
+results = sql_session.query(users).filter(users.in_room == 0).first() 
+results1 = sql_session.query(single_elim_room).filter(single_elim_room.room_number == 0).first() 
+results2 = sql_session.query(single_elim_room).filter(single_elim_room.room_number == 1).first()
 # Print the results 
 if results!= None:
     print(results) 
 else:
     print("hello")
-# Insert some data into the example table 
-#session.add(users(username = "bob", email = "dylan@gmail.com")) 
+if results1!= None:
+    print(results1) 
+else:
+    print("hello")
+if results2!= None:
+    print(results2) 
+else:
+    print("hello")
 
-#session.commit() 
+
+# Insert some data into the example table 
+
   
 # Use a mathematical equation as a filter in a query 
 #results = session.query(users).filter(email = "dylan@gmail.com").first() 
