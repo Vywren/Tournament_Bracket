0bb6c886daa6e2b81347653e6027c7cd21ba9044
Vywren <wolfiethehusky@gmail.com>
2024-10-22 23:41:16 -0400

    updated login and room finder + readme

--- flaskApp.py
+++ flaskApp.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__,template_folder = 'templates')
 app.secret_key = "dhtuisdhfu9 her79ry489fjiojf0298348idksmfoisn2rif9djf023hr8fdjf"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 engine = create_engine('sqlite:///example.db') 
 Session = sessionmaker(bind=engine) 
@@ -19,7 +19,7 @@ class single_elim_room(Base):
     def __init__(self, room_number):
         self.room_number = room_number
     def __repr__(self):
-            return f"({self.room_number})"
+            return f"{self.room_number}"
 
 class users(Base):
     __tablename__ = 'users'
@@ -33,7 +33,7 @@ class users(Base):
     def __repr__(self):
             return f"({self.email, self.username, self.in_room})"
 
-
+Base.metadata.drop_all(engine) 
 Base.metadata.create_all(engine) 
 #sql_session.add(users(email = "dylan@gmail.com")) 
 results = sql_session.query(single_elim_room).filter(single_elim_room.room_number == 0).first()
@@ -47,21 +47,57 @@ sql_session.commit()
 def do_stuff():
     return render_template('index.html')
 
-@app.route("/single_elim")
+@app.route("/single_elim", methods = ["POST", "GET"])
 def single_elim():
-    results = sql_session.query(single_elim_room).filter(single_elim_room.room_number == 0).first()
-    flash(results)
+    results = sql_session.query(single_elim_room).filter(single_elim_room.room_number > 0).all()
+    for result in results:
+        flash(str(result))
+    if request.method == "POST":
+        new = request.form.get("new_room")
+        join = request.form.get("room_num")
+        if session["user"] == None or session["email"] == None:
+            flash("Please login before joining a room")
+            return redirect(url_for("login"))
+        if new is not None:
+            #new room button was pressed
+            greatest = sql_session.query(single_elim_room).count()
+            if greatest <= 10:
+                 sql_session.add(single_elim_room(room_number = greatest)) 
+                 sql_session.commit()
+                 flash("welcome to room "+ str(greatest))
+                 return redirect(url_for("waiting_room"))
+            else:
+            #too many rooms added
+                return render_template("single_elim.html")
+        if join.isnumeric():
+            #add user to room
+            rooms = sql_session.query(single_elim_room).all()
+            for room in rooms:
+                if int(join) == int(room.room_number):
+                    session["room"] = join
+                    flash("welcome to room " + join)
+                    return redirect(url_for("waiting_room"))
+            else:            
+                flash("This is not an existing room")
+                return redirect(url_for("single_elim"))
     return render_template("single_elim.html")
 @app.route("/login",methods = ["POST", "GET"])
 def login():
     if request.method == "POST":
         email = request.form["email"]
         username = request.form["username"]
+        if username == "":
+            flash("please enter a username")
+            return redirect(url_for("login"))
+        if email == "":
+            flash("please enter an email")
+            return redirect(url_for("login"))
+        results = sql_session.query(users).filter(users.email == email).first()  
         session["email"] = email
         session["username"] = username
-        results = sql_session.query(users).filter(users.email == email).first()  
         if results != None:
             flash("Email found, Welcome Back")
+            flash (username)
             return redirect(url_for("post_log"))
             
         else:
@@ -71,6 +107,11 @@ def login():
             return redirect(url_for("post_log"))
     else:
         return render_template('login.html')
+    
+@app.route("/single_elim/waiting_room")
+def waiting_room():
+    return render_template('waiting_room.html')
+
 @app.route("/login/post_log/")
 def post_log():
     if "email" in session:
--- testbench.py
+++ testbench.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey 
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Boolean, __version__
 from sqlalchemy.orm import sessionmaker 
 from sqlalchemy.ext.declarative import declarative_base 
   
@@ -16,10 +16,12 @@ Base = declarative_base()
 class single_elim_room(Base):
     __tablename__ = 'single_elim_room'
     room_number = Column(Integer, primary_key = True)
-    def __init__(self, room_number):
+    empty = Column(Boolean)
+    def __init__(self, room_number, empty):
         self.room_number = room_number
+        self.empty = empty
     def __repr__(self):
-            return f"({self.room_number})"
+            return f"{self.room_number}, {self.empty}"
 
 class users(Base):
     __tablename__ = 'users'
@@ -37,15 +39,20 @@ class users(Base):
 Base.metadata.drop_all(engine) 
 Base.metadata.create_all(engine) 
   
-sql_session.add(single_elim_room(room_number = 0)) 
+sql_session.add(single_elim_room(room_number = 0, empty = True)) 
+sql_session.add(single_elim_room(room_number = 1, empty = True)) 
+sql_session.add(single_elim_room(room_number = 2, empty = True)) 
+sql_session.add(single_elim_room(room_number = 3, empty = True)) 
+
 sql_session.add(users(email = "dylan@gmail.com",username = "bob",in_room = 0)) 
 sql_session.commit() 
 
 
 
 results = sql_session.query(users).filter(users.in_room == 0).first() 
-results1 = sql_session.query(single_elim_room).filter(single_elim_room.room_number == 0).first() 
+results1 = sql_session.query(single_elim_room).all()
 results2 = sql_session.query(single_elim_room).filter(single_elim_room.room_number == 1).first()
+
 # Print the results 
 if results!= None:
     print(results) 
@@ -60,7 +67,11 @@ if results2!= None:
 else:
     print("hello")
 
-
+for result in results1:
+    hold = result.room_number
+    if hold == 1:
+        print(hold)
+print(__version__ )
 # Insert some data into the example table 
 
   
